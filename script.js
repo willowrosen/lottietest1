946b31686b8794b1e40e8b87c5e3ee08a397940c
willowrosen <46896291+willowrosen@users.noreply.github.com>
2019-01-21 14:24:41 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation ({
+container: document.getElementById('anim'),
+rederer: 'svg',
+loop: true,
+autoplay: true,
+path: 'https://raw.githubusercontent.com/mingukang-neyber/alicia/master/data.json',
+})
